28458a8f0500858637975785623fd7339e8aaf11
David Taylor <david.a.taylor253@gmail.com>
2022-05-17 13:56:50 -0700

    tester

--- dataPreprocess.py
+++ dataPreprocess.py
@@ -142,6 +142,6 @@ def getCentralDataset(partDataArray):
 
 if __name__ == "__main__":
     print("hello")
-    #partData = getIndividualDatasets(numbParts)
+    partData = getIndividualDatasets(numbParts)
     #normalizeParticipants(partData) #HAR data needs to be normalized
     #pooledData = getCentralDataset(partData)
